040867dc45a9ccdf9c3dcd26c576e7390497a351
Camilla Fernandes <114497536+fdscamilla@users.noreply.github.com>
2022-11-10 20:59:32 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,54 @@
+/* var decodedStringBtoA = 'Hello World!';
+
+var encodedStringBtoA = btoa(decodedStringBtoA);
+
+console.log(encodedStringBtoA);
+
+
+var decodedStringBtoA = prompt("Insira sua mensagem");
+
+var encodedStringBtoA = btoa(decodedStringBtoA);
+
+alert(encodedStringBtoA);
+
+
+// base64
+// variável da mensagem a ser codificada
+var decodedStringBtoA = prompt("Insira sua mensagem");
+
+// função de codificação
+function encode(){
+    var encodedStringBtoA = btoa(decodedStringBtoA);
+    alert(encodedStringBtoA);
+}
+
+encode();
+
+
+// variável da mensagem a ser decodificada
+var decodedStringAtoB = prompt("Insira sua mensagem");
+
+// função de decodificação
+function decode(){
+    var encodedStringAtoB = atob(decodedStringAtoB);
+    alert(encodedStringAtoB);
+}
+
+decode();
+
+
+
+// ceasar cipher
+// variável da mensagem a ser codificada
+var decodedCeasarString = prompt("Insira sua mensagem");
+
+// variável da mensagem após ser codificada
+var encodedCeasarString = decodedCeasarString.split("");
+
+
+console.log(encodedCeasarString)
+
+var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+*/
